e57ba8f663c8307646ede1b22469836d340f1ed4
BizarrIlliada <kazkoviy@icloud.com>
2023-09-12 23:20:49 +0300

    observable is done for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  // *** USE @OUTPUT ONLY WITH EVENT EMITTER, NOT WITH SUBJECT!!! ***
+  // *** SUBJECT IS ONLY FOR ACROSS COMPONENT COMMUNICATION ***
+
+  userActivated = false;
+  private activatedSub: Subscription;
+
+  constructor(
+    private userService: UserService,
+  ) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(activated => {
+      this.userActivated = activated;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
