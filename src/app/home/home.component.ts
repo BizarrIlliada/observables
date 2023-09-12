db85915bf0b56a93bc58d9d22be7e4352ea06914
BizarrIlliada <kazkoviy@icloud.com>
2023-09-12 22:21:57 +0300

    observable was created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, Observable, Observer } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // this.firstSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // })
+
+    const customIntervalObservable = Observable.create((observer: Observer<number>) => {
+      let count = 0;
+
+      const int = setInterval(() => {
+        observer.next(count);
+
+        // if (count === 3) {
+        //   observer.complete();
+        // }
+
+        count++;
+
+        if (count >= 5) {
+          observer.error(new Error('Count is ' + count));
+        }
+        console.log('Why it is continue going after unsubscribe?');
+      }, 1000)
+
+      return () => {
+        clearInterval(int);
+      }
+    })
+
+    this.firstSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => data % 2 === 0),
+        map((data: number) => 'String: ' + (data + 1))
+      )
+      .subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('Completed');
+    })
   }
 
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
+  }
 }
